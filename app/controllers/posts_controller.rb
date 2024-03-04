a275abb2e699f4cfbf73845410232b9eba192bb4
高橋宙大 <>
2024-03-04 15:29:16 +0900

    スケジュール編集画面を作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,11 +22,17 @@ class PostsController < ApplicationController
   end
 
   def edit
-    
+    @post = Post.find(params[:id])
   end
 
   def update
-    
+    @post = Post.find(params[:id])
+    if @user.update(params.require(:post).permit(:title,:start_date, :end_date, :all_day, :schedule_memo))
+      flash[:notice] = "スケジュールを更新しました"
+      redirect_to :posts
+    else
+      render "edit"
+    end
   end
 
   def destroy
