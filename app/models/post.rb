cf3b938d6f39c7d1098c0227cce60dd59fb0a3ea
高橋宙大 <>
2024-03-05 22:04:33 +0900

    CSSでの装飾完了

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
     validates  :schedule_memo ,length: { maximum: 500 }
     validate  :start_end_check
     def start_end_check
-        if !start_date.nil? && !end_date.nil? && start_date >= end_date
+        if !start_date.nil? && !end_date.nil? && start_date > end_date
             errors.add(:end_date,"は開始日より前に日付を登録できません。")
         end
     end
